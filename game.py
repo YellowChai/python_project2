b68edd72885159f9a0a97906ef0369821c196304
alice <aliceahuh213@gmail.com>
2022-09-23 15:41:39 -0400

    game design rough draft

--- game.py
+++ game.py
@@ -10,18 +10,18 @@ Game Rules:
     - the path design needed! 
 2. there are 2 monsters 
 3. player has 1 life 
-3. there are 6 items total:
+4. there are 6 items total:
     {a. medicine - can use it after the first monster attack, get one life back  
-    b. weapon - can use when monster met(can get rid of one of the monsters),
+    b. weapon - can use when the player met monster(can get rid of one of the monsters),
     c. key - need it to open the secret door 
     d. map - can look at the map (user can get this in very early stage? or just display the map everytime without for default? ) 
     e. helper - user can ask how many steps the player is away from the monster 
     f. helper2 - user can ask if there is a pet or a monster in certain room }
-4. 
+ 
 
 Game ends when:
 1. the player found the pet and key and also found the secret door
-2. the player met monsters 3 times total. 
+2. the player lost the life 
 3. the player can give up during the game
 
 
