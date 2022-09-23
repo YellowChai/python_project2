d12dfa16ba9535e034d368fec7fad0175a1f6cc3
alice <aliceahuh213@gmail.com>
2022-09-23 17:06:13 -0400

    game map diagram for reference

--- game.py
+++ game.py
@@ -1,28 +1,5 @@
-'''Game design:
-SAVE YOUR PET! or PETE ? 
-
-About game 
-- player needs to find his pet and key and escape through the secret door. 
-
-Game Rules: 
-
-1. there are 10 rooms 
-    - the path design needed! 
-2. there are 2 monsters 
-3. player has 1 life 
-4. there are 6 items total:
-    {a. medicine - can use it after the first monster attack, get one life back  
-    b. weapon - can use when the player met monster(can get rid of one of the monsters),
-    c. key - need it to open the secret door 
-    d. map - can look at the map (user can get this in very early stage? or just display the map everytime without for default? ) 
-    e. helper - user can ask how many steps the player is away from the monster 
-    f. helper2 - user can ask if there is a pet or a monster in certain room }
- 
-
-Game ends when:
-1. the player found the pet and key and also found the secret door
-2. the player lost the life 
-3. the player can give up during the game
+'''
+pseudo code 
 
 
 '''
